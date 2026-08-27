050a1369b239d188014a1677d6aed98056e6272e
demo <licheng2010@gmail.com>
2026-08-28 01:51:07 +0800

    默域名xdeskdev.com以及服务器 Key:CoQBOwob48IBzrXEjYWvR0mBZBW8K2zaAvytboHIn88=

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -156,8 +156,10 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+//pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+//pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xdeskdev.com"];
+pub const RS_PUB_KEY: &str = "CoQBOwob48IBzrXEjYWvR0mBZBW8K2zaAvytboHIn88=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
